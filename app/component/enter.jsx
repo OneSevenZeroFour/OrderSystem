ee3601c7ac707548dd77eaa08c49f64eb13e969d
Adaer <761979525@qq.com>
2017-10-23 15:11:34 +0800

    修改入口样式，完成逻辑处理，路由跳转

--- app/component/enter.jsx
+++ app/component/enter.jsx
@@ -1,17 +1,70 @@
 import React from "react";
 import "../../css/enter.css";
+
 export default class Renter extends React.Component{
 	constructor(props){
         super(props);
+        this.state = {
+        	desk_box:[],
+        	flag:false
+        };
+        this.popmenu = ()=>{
+        	console.log(this)
+        	this.refs.pops.style.display = "block";
+        }
+        this.cancelpop = ()=>{
+        	this.refs.pops.style.display = "none";
+        }
+        this.write_desk_num = (event)=>{
+        	//存入本地存储
+        	console.log(event.target.dataset)
+        	var num = event.target.dataset.desker;
+        	window.localStorage.setItem("desk_num",num);
+        }
+	}
+	componentWillMount(){
+		var self = this;
+		window.$.ajax({
+			url:"http://10.3.132.65:10002/desk",
+			type:"GET",
+			success(datas){
+				var res = datas;
+				try{
+					res = JSON.parse(datas)
+				}catch(err){
+					console.log(err)
+				}
+				console.log(res)
+				var res2=res.results.map(function(item){
+					return item.desk;
+				})
+				self.setState({
+					desk_box:res2.slice()
+				})
+			}
+		})
 	}
 	render(){
 		return (
 			<header className="enter_hd">
 				<div className="enter_bg"></div>
 				<div className="enter_box">
-					<a href="javascript:;">客人入口</a>
+					<a href="javascript:;" onClick={this.popmenu}>客人入口</a>
 					<a href="javascript:;">服务入口</a>
 					<a href="javascript:;">后厨入口</a>
+				</div>
+				<div className="enter_desk" ref="pops">
+					<ul>
+						<button onClick={this.cancelpop}>&times;</button>
+						{
+							this.state.desk_box.map(function(item,idx){
+								var n = idx+1;
+		        				return <li key={item}>
+		        						<a href="#/user" data-desker={n} onClick={this.write_desk_num}>{item}</a>
+		        						</li>;
+		        			}.bind(this))
+						}
+					</ul>
 				</div>
 			</header>
 		)
--- index.js
+++ index.js
@@ -14,13 +14,32 @@ import store from "./store.js";
 import {
 	Provider
 } from "react-redux";
+// import {
+// 	Router,
+// 	Route,
+// 	hashHistory
+// } from 'react-router';
+import {
+	HashRouter,
+	Route,
+	Link
+} from 'react-router-dom'
+import $ from "jquery";
+window.$ = $;
+
+
 import "./css/base.css";
 import User from "./app/component/user.jsx";
 import Renter from "./app/component/enter.jsx";
 
 var element = (
+	<HashRouter>
+	<Provider store={store}>
 	<div>
-	<Renter />
+		// <Route path="*" component={Renter} />
+		<Route exact path="/" component={Renter} />
 	</div>
+	</Provider>
+	</HashRouter>
 );
 ReactDOM.render(element, document.getElementById("demo"));
